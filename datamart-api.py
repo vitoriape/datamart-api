fd496468ddf6d09f07ccf6f92201e16a920cf1af
vitoriape <vitoriapecanha.log@gmail.com>
2025-07-11 17:41:25 -0300

    Change ODBC driver

--- datamart-api.py
+++ datamart-api.py
@@ -251,11 +251,12 @@ def registrar_hotspot(
 # SQL Server connection configs
 def get_sqlserver_connection():
     conn_str = (
-        f"DRIVER={{ODBC Driver 17 for SQL Server}};"
+        f"DRIVER={{ODBC Driver 18 for SQL Server}};"
         f"SERVER={os.getenv('SQL_SERVER_HOST')};"
         f"DATABASE={os.getenv('SQL_SERVER_DB')};"
         f"UID={os.getenv('SQL_SERVER_USER')};"
         f"PWD={os.getenv('SQL_SERVER_PASSWORD')};"
+        f"Encrypt=yes;"
         f"TrustServerCertificate=yes;"
     )
     return pyodbc.connect(conn_str)
